472d32daf7039a27dd19137a9d982d1da11e2259
trianglesis <it@trianglesis.org.ua>
2020-09-02 15:15:57 +0300

    Getting new efficient fac with go function

--- recursions/tail_recursion.py
+++ recursions/tail_recursion.py
@@ -6,18 +6,27 @@ from examples.factorial import get_recursive_factorial
 
 number = 5
 # Factorial Inefficient
-rec_fact = get_recursive_factorial(number)
-print(rec_fact)
+# rec_fact = get_recursive_factorial(number)
+# print(rec_fact)
 
 
 # Factorial efficient with "go" function:
+#  Just as example where a is an accumulator or calculation step by step
 def go(n, a):
     # go (n - 1) (a * n)
     while n > 0:
         a = a * n
         n = n - 1
     return a
+# fact = go(number, 1)
+# print(fact)
 
 
-fact = go(number, 1)
-print(fact)
+# Fib example
+def fib_f(n):
+    a = n - 1
+    b = a + n
+    return(a,b)
+
+fib = fib_f(4)
+print(fib)
